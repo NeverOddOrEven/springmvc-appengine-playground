a983a008aeed3511f360b100cf38f6c462cfb189
unknown <Alex@Apollo-PC.(none)>
2013-04-11 22:10:27 -0400

    All the controller routes work now

--- src/com/mobile/controllers/HomeController.java
+++ src/com/mobile/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller("mobileHomeController")
-public class MHomeController {
+public class HomeController {
  
 	@RequestMapping("/mobile")
 	public ModelAndView home(ModelMap model) {
